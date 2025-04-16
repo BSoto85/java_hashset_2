5d5ddf8641e5f51812ab93d330cdb4385b9f5a5a
brenda-soto <bsoto@pursuit.org>
2025-04-16 16:58:31 -0400

    Finished java_hashset_2

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -3,5 +3,19 @@ package org.example; // Make sure this matches your package
 import java.util.Objects; // For equals() and hashCode()
 
 public class Car {
+    String model;
+    String make;
 
+    public Car(String model, String make) {
+        this.model = model;
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getMake() {
+        return make;
+    }
 }
--- src/main/java/org/example/CarService.java
+++ src/main/java/org/example/CarService.java
@@ -2,8 +2,39 @@ package org.example; // Make sure this matches your package
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CarService {
+    Set<Car> cars = new HashSet<>();
 
+    public void addCar(Car car) {
+        if (car == null) {
+            throw new IllegalArgumentException("Car cannot be null");
+        } else {
+            cars.add(car);
+        }
+    }
+
+    public Boolean removeCar(Car car) {
+        if (car == null) {
+            throw new IllegalArgumentException("Car cannot be null");
+        } else {
+            return cars.remove(car);
+        }
+    }
+
+    public int getCarCount() {
+        return cars.size();
+    }
+
+    public Set<Car> getCarsByMake(String make) {
+        if (make == null || make.isEmpty()) {
+            throw new IllegalArgumentException("Make cannot be null or empty");
+        } else {
+            return cars.stream()
+                    .filter(car -> car.getMake()
+                            .equalsIgnoreCase(make)).collect(Collectors.toSet());
+        }
+    }
 
 }
